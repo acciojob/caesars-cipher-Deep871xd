91aa6337000ec045c90f55b924b79d58f87998b5
Deep871xd <102525444+Deep871xd@users.noreply.github.com>
2023-06-08 19:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// Your Script here.
-
 const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
@@ -11,26 +9,21 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   // empty string to store the decoded result
-  let decodedString = [];
-	var arWords=encodedStr.split(" ");
-  // Iterate over each character in the encoded string
-  for (let i = 0; i < arWords.length; i++) {
-    // Get the ASCII code of the current character
-    var newStr="";
-	  var strWord=arWords[i];
-	  for(var j=0;j<strWord.length;j++){
-		  newStr+=lookup[strWord.charAt(j)];
-	  }
-	  decodedString.push(newStr);
-  }
+   let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+	for(let i = 0 ; i < encodedStr.length ; i++) {
+        if(lookup[ encodedStr[i] ] === undefined){
+            decoderArr.push( encodedStr[i] ) ;
+        }   
+        else{ 
+            decoderArr.push(lookup[ encodedStr[i] ]);
+        }
 
-  // Return the decoded string
-  return decodedString.join(" ");
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
